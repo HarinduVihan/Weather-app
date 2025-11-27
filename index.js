9d3b6d6d41cc150ce9a6fdc7dab456d757c9dfe6
HarinduVihan <harinduvihan@gmail.com>
2025-11-27 13:08:16 +0530

    get futuer 24 hours

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function checkWeather(city) {
     document.querySelector(".error").style.display = "none";
     document.querySelector(".loader").style.display = "none";
 
-    //checkForcast(city);
+    checkForcast(city);
   }
 }
 
@@ -118,15 +118,13 @@ async function checkForcast(city) {
   document.querySelector(".yesterday").innerHTML = data.days[0].conditions;
   document.querySelector(".tommorow").innerHTML = data.days[2].conditions;
 
-  document.querySelector(".hour0").innerHTML = data.days[1].hours[0].datetime;
-  document.querySelector(".temp0").innerHTML = data.days[1].hours[0].temp;
-  document.querySelector(".weather0").innerHTML =
-    data.days[1].hours[0].conditions;
-
-  document.querySelector(".hour1").innerHTML = data.days[1].hours[1].datetime;
-  document.querySelector(".temp1").innerHTML = data.days[1].hours[1].temp;
-  document.querySelector(".weather1").innerHTML =
-    data.days[1].hours[1].conditions;
+  for (let i = 0; i < 24; i++) {
+    document.querySelector(".hour" + i).innerHTML =
+      data.days[1].hours[i].datetime;
+    document.querySelector(".temp" + i).innerHTML = data.days[1].hours[i].temp;
+    document.querySelector(".weather" + i).innerHTML =
+      data.days[1].hours[i].conditions;
+  }
 }
 
 searchBtn.addEventListener("click", () => {
